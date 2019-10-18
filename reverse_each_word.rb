898d26a3bd7bf3d97212d46aca5c0fb856fe9e05
Tavis Weeden <tavisweeden@gmail.com>
2019-10-18 22:00:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 require "pry"
 def reverse_each_word(sentence)
  newsentence = sentence.split(" ") 
- newsentence.each do |word| 
+ newsentence.each do |word|
+   word.reverse!
  binding.pry   
   end
 end
